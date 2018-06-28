d4d56657e8571260d41fcaa52cf6962fc28dc61f
Randal Kamradt (CW) <rkamradt@rkamradt006043.starbucks.net>
2018-06-28 16:20:23 -0700

    remove -it to allow automation

--- docker-run.sh
+++ docker-run.sh
@@ -5,7 +5,7 @@ export RESULTS_DIR=${DIR}/results
 
 rm -rf ${RESULTS_DIR}
 
-docker run -it --rm -v ${CONF_DIR}:/opt/gatling/conf \
+docker run --rm -v ${CONF_DIR}:/opt/gatling/conf \
   -v ${USER_DIR}:/opt/gatling/user-files \
   -v ${RESULTS_DIR}:/opt/gatling/results \
   denvazh/gatling -s naiveuser.BasicSimulation -m
