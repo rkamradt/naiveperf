1da7728fa5699273e4a015e495168c48a1dfbb90
Randal Kamradt (CW) <rkamradt@rkamradt006043.starbucks.net>
2018-06-29 08:54:21 -0700

    added logging to docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -5,6 +5,10 @@ export RESULTS_DIR=${DIR}/results
 
 rm -rf ${RESULTS_DIR}
 
+echo user-files: ${USER_DIR}
+date
+ls -la ${USER_DIR}/simulations/naiveuser
+
 docker run --rm -v ${CONF_DIR}:/opt/gatling/conf \
   -v ${USER_DIR}:/opt/gatling/user-files \
   -v ${RESULTS_DIR}:/opt/gatling/results \
